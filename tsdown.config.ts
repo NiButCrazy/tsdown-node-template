ef1ce5e891ca6cdfc74c6a9eb6c223748adfc159
NiButCrazy <3104955349@qq.com>
2025-07-21 11:41:02 +0800

    build: 小修

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,6 +5,6 @@ export default defineConfig([
   {
     entry: [ './src/index.ts' ],
     platform: 'node',
-    dts: true, // 控制是否生成生成 .d.ts 声明文件
+    dts: false, // 控制是否生成生成 .d.ts 声明文件，建议在 build 阶段添加 --dts 生成，这里 dev 阶段设置为 false
   },
 ])
